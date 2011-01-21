7928f0cb244068529fbbf992010103d313795de5
trbngr <chris@caliberweb.com>
2011-01-21 09:50:35 -0700

    playing with openfilesystem

--- src/Caliberweb.Core/Extensions/OpenFileSystemExtensions.cs
+++ src/Caliberweb.Core/Extensions/OpenFileSystemExtensions.cs
@@ -1,12 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 
 using OpenFileSystem.IO;
+using OpenFileSystem.IO.FileSystems.Local;
 
 namespace Caliberweb.Core.Extensions
 {
-    public static class OpenFileSystemFileExtensions
+    public static class OpenFileSystemExtensions
     {
         public static string[] ReadAllLines(this IFile file)
         {
@@ -38,6 +41,26 @@ namespace Caliberweb.Core.Extensions
                     writer.WriteLine(line);
                 }
             }
+        }
+
+        public static DateTime LastWriteTimeUtc(this IDirectory directory)
+        {
+            var type = typeof(LocalDirectory);
+
+            var info = type.GetProperty("DirectoryInfo",
+                                        BindingFlags.NonPublic |
+                                        BindingFlags.GetProperty |
+                                        BindingFlags.SetProperty |
+                                        BindingFlags.Instance);
+
+            var di = info.GetValue(directory, null) as DirectoryInfo;
+
+            if (di != null)
+            {
+                return di.LastWriteTimeUtc;
+            }
+
+            return DateTime.MinValue;
         }
     }
 }
--- src/ConsoleDriver/Runner.cs
+++ src/ConsoleDriver/Runner.cs
@@ -1,69 +1,98 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 
 using Caliberweb.Core;
+using Caliberweb.Core.Specification;
 using Caliberweb.Core.Verizon;
 
 using ConsoleDriver.Internal;
 
 using log4net;
 
+using OpenFileSystem.IO;
+using OpenFileSystem.IO.FileSystems.Local;
+
+using Path = System.IO.Path;
+
 using System.Linq;
 
-using OpenFileSystem.IO.FileSystems.InMemory;
-using OpenFileSystem.IO.FileSystems.Local;
+using Caliberweb.Core.Extensions;
 
 namespace ConsoleDriver
 {
     internal class Runner : IRunner
     {
         private static readonly ILog log = LogManager.GetLogger(typeof (Runner));
-        private readonly ConsoleReader reader;
         private readonly LocalFileSystem fs;
+        private readonly ConsoleReader reader;
 
         public Runner(ConsoleReader reader)
         {
             this.reader = reader;
             fs = LocalFileSystem.Instance;
         }
-
+        
         #region IRunner Members
 
         public void Run()
         {
-            const string PATH = @"C:\Users\cmartin.NTSPHX1\Downloads\phonecalls";
-//          
-            var file = fs.GetFile(Path.Combine(PATH, "6023265200.csv"));
+            var backupDirectory = fs.GetDirectory(@"D:\!saved");
 
-            Console.Out.WriteLine(file);
-            
+            var dt = new DateTime(2010, 9, 3);
+            var dirSpec = Spec<IDirectory>.Create(d => d.LastWriteTimeUtc() > dt);
 
-                        var minutes = new VerizonMinutesReader(new[]
-                        {
-                            fs.GetFile(Path.Combine(PATH, "6023265200.csv")),
-                            fs.GetFile(Path.Combine(PATH, "6025096418.csv")),
-                            fs.GetFile(Path.Combine(PATH, "6025096464.csv")),
-                            fs.GetFile(Path.Combine(PATH, "6025097062.csv")),
-                        });
-            
-                        var spec = VerizonRecord.NumberIsNot("Misty")
-                            .And(VerizonRecord.NotTollFree)
-                            .And(VerizonRecord.MinutesGreaterThan(9));
-            
-                        foreach (var record in minutes.GetFriendsAndFamilyRecommendations(spec))
-                        {
-                            log.Info(record);
-                        }
-            
-                        foreach (var record in minutes.GroupByNumber())
-                        {
-                            log.Info(record);
-                        }
+            var fileSpec = Spec<IFile>.Create(f => f.LastModifiedTimeUtc > dt)
+                .And(f => f.Extension == ".mp3");
+
+            var files = fs.GetDirectory(@"D:\!music")
+                .Directories()
+                .Where(dirSpec.IsSatisfied)
+                .SelectMany(d => d.Files("*", SearchScope.SubFolders)
+                                     .Where(fileSpec.IsSatisfied))
+                .ToList();
+
+            foreach (var file in files)
+            {
+                var pathQueue = new Queue<string>(file.Path.Segments);
+
+                pathQueue.Dequeue();
+                pathQueue.Dequeue();
+
+                var remainingPath = pathQueue.Aggregate("", (p, c) => string.Concat(p, c, "/")).TrimEnd('/');
+
+                var newPath = Path.Combine(backupDirectory.Path.FullPath, remainingPath);
+
+                var dest = fs.GetFile(newPath);
+                
+                file.CopyTo(dest);
+            }
+            //            const string PATH = @"C:\Users\cmartin.NTSPHX1\Downloads\phonecalls";
+//
+//            var minutes = new VerizonMinutesReader(new[]
+//            {
+//                fs.GetFile(Path.Combine(PATH, "6023265200.csv")),
+//                fs.GetFile(Path.Combine(PATH, "6025096418.csv")),
+//                fs.GetFile(Path.Combine(PATH, "6025096464.csv")),
+//                fs.GetFile(Path.Combine(PATH, "6025097062.csv")),
+//            });
+//
+//            ISpec<VerizonRecord> spec = VerizonRecord.NumberIsNot("Misty")
+//                .And(VerizonRecord.NotTollFree)
+//                .And(VerizonRecord.MinutesGreaterThan(9));
+//
+//            foreach (VerizonRecord record in minutes.GetFriendsAndFamilyRecommendations(spec))
+//            {
+//                log.Info(record);
+//            }
+//
+//            foreach (VerizonRecord record in minutes.GroupByNumber())
+//            {
+//                log.Info(record);
+//            }
         }
 
         #endregion
-
-
     }
-
 }
